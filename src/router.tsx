74ae22ef8fe94a8b04121dfcaff1621848edaf99
Mungums <maxsboris18russ@gmail.com>
2026-05-08 19:40:14 +0400

    fix dasboard display

--- src/router.tsx
+++ src/router.tsx
@@ -35,7 +35,7 @@ export const router = createBrowserRouter([
       </ProtectedRoute>
     ),
     children: [
-      {
+            {
         path: '/profile',
         element: <ProfilePage />,
       },
